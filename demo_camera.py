3927c3fffb58dffbfcb5cb2b49ddd1d08eeb4ba9
Dzmitry Kamarouski <dzkamarouski@gmail.com>
2019-02-11 22:10:24 +0300

    Update demo_camera.py

--- demo_camera.py
+++ demo_camera.py
@@ -1,1 +1,198 @@
 # TODO
+import keras
+from keras.models import Sequential
+from keras.models import Model
+from keras.layers import Input, Dense, Activation, Lambda
+from keras.layers.convolutional import Conv2D
+from keras.layers.pooling import MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+from keras.layers.merge import Concatenate
+from config_reader import config_reader
+import scipy
+import math
+
+def relu(x): 
+    return Activation('relu')(x)
+
+def conv(x, nf, ks, name):
+    x1 = Conv2D(nf, (ks, ks), padding='same', name=name)(x)
+    return x1
+
+def pooling(x, ks, st, name):
+    x = MaxPooling2D((ks, ks), strides=(st, st), name=name)(x)
+    return x
+
+def vgg_block(x):
+     
+    # Block 1
+    x = conv(x, 64, 3, "conv1_1")
+    x = relu(x)
+    x = conv(x, 64, 3, "conv1_2")
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool1_1")
+
+    # Block 2
+    x = conv(x, 128, 3, "conv2_1")
+    x = relu(x)
+    x = conv(x, 128, 3, "conv2_2")
+    x = relu(x)
+    x = pooling(x, 2, 2, "pool2_1")
+    
+    # Block 3
+    x = conv(x, 256, 3, "conv3_1")
+    x = relu(x)    
+    x = conv(x, 256, 3, "conv3_2")
+    x = relu(x)    
+    x = conv(x, 256, 3, "conv3_3")
+    x = relu(x)    
+    x = conv(x, 256, 3, "conv3_4")
+    x = relu(x)    
+    x = pooling(x, 2, 2, "pool3_1")
+    
+    # Block 4
+    x = conv(x, 512, 3, "conv4_1")
+    x = relu(x)    
+    x = conv(x, 512, 3, "conv4_2")
+    x = relu(x)    
+    
+    # Additional non vgg layers
+    x = conv(x, 256, 3, "conv4_3_CPM")
+    x = relu(x)
+    x = conv(x, 128, 3, "conv4_4_CPM")
+    x = relu(x)
+    
+    return x
+
+def stage1_block(x, num_p, branch):
+    
+    # Block 1        
+    x = conv(x, 128, 3, "conv5_1_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, 128, 3, "conv5_2_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, 128, 3, "conv5_3_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, 512, 1, "conv5_4_CPM_L%d" % branch)
+    x = relu(x)
+    x = conv(x, num_p, 1, "conv5_5_CPM_L%d" % branch)
+    
+    return x
+
+def stageT_block(x, num_p, stage, branch):
+        
+    # Block 1        
+    x = conv(x, 128, 7, "Mconv1_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv2_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv3_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv4_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 7, "Mconv5_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, 128, 1, "Mconv6_stage%d_L%d" % (stage, branch))
+    x = relu(x)
+    x = conv(x, num_p, 1, "Mconv7_stage%d_L%d" % (stage, branch))
+    
+    return x
+
+
+
+weights_path = "/home/dmitry.kamarouski/work/rd/flips/video_classification/weights/model_base.h" # orginal weights converted from caffe
+#weights_path = "training/weights.best.h5" # weights tarined from scratch 
+
+input_shape = (None,None,3)
+
+img_input = Input(shape=input_shape)
+
+stages = 6
+np_branch1 = 38
+np_branch2 = 19
+
+img_normalized = Lambda(lambda x: x / 256 - 0.5)(img_input)  # [-0.5, 0.5]
+
+# VGG
+stage0_out = vgg_block(img_normalized)
+
+# stage 1
+stage1_branch1_out = stage1_block(stage0_out, np_branch1, 1)
+stage1_branch2_out = stage1_block(stage0_out, np_branch2, 2)
+x = Concatenate()([stage1_branch1_out, stage1_branch2_out, stage0_out])
+
+# stage t >= 2
+for sn in range(2, stages + 1):
+    stageT_branch1_out = stageT_block(x, np_branch1, sn, 1)
+    stageT_branch2_out = stageT_block(x, np_branch2, sn, 2)
+    if (sn < stages):
+        x = Concatenate()([stageT_branch1_out, stageT_branch2_out, stage0_out])
+
+model = Model(img_input, [stageT_branch1_out, stageT_branch2_out])
+model.load_weights(weights_path)
+
+import cv2
+import matplotlib
+import pylab as plt
+import numpy as np
+import util
+
+vc = cv2.VideoCapture(0)
+while True:
+    _, oriImg = vc.read() # B,G,R order
+    #plt.imshow(oriImg[:,:,[2,1,0]])
+    
+
+    param, model_params = config_reader()
+
+    multiplier = [x * model_params['boxsize'] / oriImg.shape[0] for x in param['scale_search']]
+
+
+    heatmap_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 19))
+    paf_avg = np.zeros((oriImg.shape[0], oriImg.shape[1], 38))
+
+    input_img = np.transpose(np.float32(imageToTest_padded[:,:,:,np.newaxis]), (3,0,1,2)) 
+    output_blobs = model.predict(input_img)
+
+    heatmap = np.squeeze(output_blobs[1]) 
+    paf = np.squeeze(output_blobs[0])
+
+    heatmap_avg = heatmap
+    paf_avg = paf
+    
+    from scipy.ndimage.filters import gaussian_filter
+    all_peaks = []
+    peak_counter = 0
+
+    for part in range(19-1):
+        map_ori = heatmap_avg[:,:,part]
+        map = gaussian_filter(map_ori, sigma=3)
+    
+        map_left = np.zeros(map.shape)
+        map_left[1:,:] = map[:-1,:]
+        map_right = np.zeros(map.shape)
+        map_right[:-1,:] = map[1:,:]
+        map_up = np.zeros(map.shape)
+        map_up[:,1:] = map[:,:-1]
+        map_down = np.zeros(map.shape)
+        map_down[:,:-1] = map[:,1:]
+    
+        peaks_binary = np.logical_and.reduce((map>=map_left, map>=map_right, map>=map_up, map>=map_down, map > param['thre1']))
+        peaks = list(zip(np.nonzero(peaks_binary)[1], np.nonzero(peaks_binary)[0])) # note reverse
+        peaks_with_score = [x + (map_ori[x[1],x[0]],) for x in peaks]
+        id = range(peak_counter, peak_counter + len(peaks))
+        peaks_with_score_and_id = [peaks_with_score[i] + (id[i],) for i in range(len(id))]
+
+        all_peaks.append(peaks_with_score_and_id)
+        peak_counter += len(peaks)
+
+    limbSeq = [[2,3], [2,6], [3,4], [4,5], [6,7], [7,8], [2,9], [9,10], \
+           [10,11], [2,12], [12,13], [13,14], [2,1], [1,15], [15,17], \
+           [1,16], [16,18], [3,17], [6,18]]
+# the middle joints heatmap correpondence
+    mapIdx = [[31,32], [39,40], [33,34], [35,36], [41,42], [43,44], [19,20], [21,22], \
+          [23,24], [25,26], [27,28], [29,30], [47,48], [49,50], [53,54], [51,52], \
+          [55,56], [37,38], [45,46]]
+
+
+
+
